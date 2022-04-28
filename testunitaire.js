0cfcc633bff67e66bcef6cd83a10233c531673b9
Mysteriou85 <guib5@hotmail.fr>
2022-04-28 12:09:34 +0200

    add calcul prix

--- testunitaire.js
+++ testunitaire.js
@@ -1,23 +1,47 @@
 
 let panier = [];
 
-function createProduit(nom, prix, poids, couleur, taille) {
-    let produit = { nom: nom, prix: prix, poids: poids, couleur: couleur, taille: taille }
+function createarticle(nom, prix, poids, couleur, taille) {
+    let article = { id: `${nom}_${taille}_${couleur}`, nom: nom, prix: prix, poids: poids, couleur: couleur, taille: taille }
 
-    return produit;
+    return article;
 }
 
 function ajoutPanier(produit, quantite) {
     panier.push({ produit: produit, quantite: quantite })
-    //doit ajouté 'if' pour vérifier si le produit exite déjà et fusionné avec produit identique
+    //A FAIRE : doit ajouté 'if' pour vérifier si le produit exite déjà et fusionné avec produit identique
+    // console.log(panier.includes(produit));
+
+    // const sameVerif = article.id;
+
+    // if (sameVerif = article.id) {
+    //     console.log('=====> ça passe ici')
+    // }
 }
 
-let tshirt = createProduit('T-shirt', 10, 0.5, 'rouge', 'M');
+function prixTotal(panier) {
+    let totalPrix = 0;
+
+    console.log(panier);
+
+    panier.forEach(article => {
+        //calcul prix article * quantité
+        totalPrix += article.produit.prix * article.quantite
+        //calcul frais de port
+        totalPrix = totalPrix * (article.produit.poids * 1.2)
+    })
+    //ajout TVA 20%
+    totalPrixTCC= totalPrix * 1.2;
+    console.log(`Prix total : ${totalPrixTCC}`)
+}
+
+let tshirt = createarticle('T-shirt', 10, 0.5, 'rouge', 'M');
 ajoutPanier(tshirt, 1);
 
-let pull = createProduit('Pull', 15, 1, 'violet', 'XL');
+let pull = createarticle('Pull', 15, 1, 'violet', 'XL');
 ajoutPanier(pull, 2);
 
-console.log(panier);
+prixTotal(panier)
 
-//panier = [{produit: produit, quantité: 2}];
+//console.log(panier);
+
