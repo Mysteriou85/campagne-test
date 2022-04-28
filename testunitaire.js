aa48b60bf89c6583c6ccb8f66f8cfd5db1dbf4ee
Mysteriou85 <guib5@hotmail.fr>
2022-04-28 11:36:02 +0200

    create produit, add produit

--- testunitaire.js
+++ testunitaire.js
@@ -1,4 +1,23 @@
 
 let panier = [];
 
-let produit = { nom: 'NomProduit', prix: '15', poids: '0.5', couleur: 'rouge', taille: 'XL'}
+function createProduit(nom, prix, poids, couleur, taille) {
+    let produit = { nom: nom, prix: prix, poids: poids, couleur: couleur, taille: taille }
+
+    return produit;
+}
+
+function ajoutPanier(produit, quantite) {
+    panier.push({ produit: produit, quantite: quantite })
+    //doit ajouté 'if' pour vérifier si le produit exite déjà et fusionné avec produit identique
+}
+
+let tshirt = createProduit('T-shirt', 10, 0.5, 'rouge', 'M');
+ajoutPanier(tshirt, 1);
+
+let pull = createProduit('Pull', 15, 1, 'violet', 'XL');
+ajoutPanier(pull, 2);
+
+console.log(panier);
+
+//panier = [{produit: produit, quantité: 2}];
